75432f6d33d50cff481ba97537e1a87e37b11ec5
Ming <zhangming0411@foxmail.com>
2016-10-24 10:00:08 +0800

    添加了login、register的html文件

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,29 @@
 # -*- coding:utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email, EqualTo
+from wtforms.validators import Required, Length, Email, EqualTo, Regexp
+from ..models import User
+from wtforms import ValidationError
 
 class LoginForm(Form):
-    email = StringField(u'邮箱', validators = [Required(), Length(1, 64),
-                        Email()])
+    email = StringField(u'邮箱', validators = [Required(), Length(1, 64), Email()])
     password = PasswordField(u'密码', validators = [Required()])
     remember_me = BooleanField(u'保持登录')
     submit = SubmitField(u'登录')
+
+class RegistrationForm(Form):
+    email = StringField(u'邮箱', validators = [Required(), Length(1, 64),
+                Email()])
+    username = StringField(u'用户名', validators = [Required(), Length(1, 64)])
+    password = PasswordField(u'密码', validators = [Required(), Length(1, 64),
+                EqualTo('password2', message = u"密码必须保持一致")])
+    password2 = PasswordField(u'重复密码', validators = [Required()])
+    submit = SubmitField(u'注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email = field.data).first():
+            raise ValidationError(u'邮箱已被注册')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username = field.data).first():
+            raise ValidationError(u'用户名已被使用')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, flash
 from . import main
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
 from flask_login import login_user, logout_user, login_required, \
     current_user
 from ..models import User
@@ -19,6 +19,18 @@ def login():
         user = User.query.filter_by(email = form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
-            return url_for('index')
+            return redirect(url_for('.index'))
         flash(u'欢迎回来！')
     return render_template('login.html', form = form)
+
+@main.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.login'))
+    return render_template('register.html', form = form)
